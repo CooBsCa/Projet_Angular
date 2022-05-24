6b9cc12b959f40eeb17db412b3bed6bbc81745d8
CooBsCa <cheminbastien@gmail.com>
2022-05-24 12:58:08 +0200

    Exercice 6
    Liste,
    Petit correctif j'avais fait avec un tableau trop tôt

--- statistiques/src/app/app.component.ts
+++ statistiques/src/app/app.component.ts
@@ -7,10 +7,10 @@ import { Statistique} from 'src/models/statistique';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public mesStats: Statistique[] = [
-    {identifiant: "fa1f5f40-be3b-11eb-91ec-7f5875ecfb46", titre: "Démographie en France",valeur:"60M"},
-    {identifiant: "fa1f5f40-be3b-11eb-91ec-7f5875ecfb46", titre: "Démographie en Europe",valeur:"80M"},
-];
   title = 'statistiques';
+  public stat1: Statistique = {identifiant: "fa1f5f40-be3b-11eb-91ec-7f5875ecfb46", titre: "Démographie en France",valeur:"60M"};
+  public stat2: Statistique = {identifiant: "fa1f5f40-be3b-11eb-91ec-7f5875ecfb46", titre: "Démographie en Europe",valeur:"80M"};
+  public mesStats: Statistique[] = [this.stat1,this.stat2];
+  
 }
 
