ee2d41726ddc52c312ee293ae81ab457f1d79129
CooBsCa <cheminbastien@gmail.com>
2022-05-24 12:27:01 +0200

    Exercice 4
    Ajout de stats en dure

--- statistiques/src/app/app.component.ts
+++ statistiques/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { statistique } from 'src/models/statistique';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public mesStats: statistique[] = [
+    {identifiant: "fa1f5f40-be3b-11eb-91ec-7f5875ecfb46", titre: "Démographie en France",valeur:"60M"},
+    {identifiant: "fa1f5f40-be3b-11eb-91ec-7f5875ecfb46", titre: "Démographie en Europe",valeur:"80M"},
+];
   title = 'statistiques';
 }
+
